138b92432b83aad9128015a6e8e51c5419c97d24
Preecha Nakngern <preecha@readyplanet.com>
2016-05-05 22:54:17 +0700

    update project directory

--- gruntfile.js
+++ gruntfile.js
@@ -10,15 +10,43 @@ module.exports = function (grunt) {
                     'public/css/index.css': 'resource/scss/index.scss'
                 }
             }
+        },
+        copy: {
+            libs: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'node_modules/angular/',
+                        src: ['angular.min.js'],
+                        dest: 'public/js/libs/angular'
+                    }, {
+                        expand: true,
+                        cwd: 'node_modules/requirejs/',
+                        src: ['require.js'],
+                        dest: 'public/js/libs/require'
+                    }, {
+                        expand: true,
+                        cwd: 'node_modules/angular-ui-bootstrap/dist/',
+                        src: ['ui-bootstrap-csp.css'],
+                        dest: 'public/css/libs/angular-ui-bootstrap'
+                    }, {
+                        expand: true,
+                        cwd: 'node_modules/bootstrap/dist/css/',
+                        src: ['bootstrap.min.css'],
+                        dest: 'public/css/libs/bootstrap'
+                    }
+                ]
+            }
         }
     });
 
 	//load plugins
 	grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     //create css style sheet
-    grunt.registerTask('default', 'run application on android with source', function() {
-        grunt.task.run(['sass:pub']);
+    grunt.registerTask('default', 'create env for deploy web', function() {
+        grunt.task.run(['sass:pub', 'copy:libs']);
     });
 
 };
