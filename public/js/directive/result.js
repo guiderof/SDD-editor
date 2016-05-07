a6616dfa1950658fb07ac00043ce942c023be2e8
Preecha Nakngern <preecha@readyplanet.com>
2016-05-07 16:16:23 +0700

    update UI

--- public/js/directive/result.js
+++ public/js/directive/result.js
@@ -17,17 +17,49 @@ define([
 					$scope.assignment_id = "";
 					//$rootScope.params.question_id
                     $scope.submit = function () {
-						if ($scope.attempt_id && $scope.question_id) {
-							if ($scope.point) {
-
-							}
+						if ($scope.assignment_id && $scope.question_id) {
+							var path = "path to submit result of compile";
+							$http({
+		                        method: 'POST',
+		                        url: path,
+		                        headers: {
+		                            "Content-type": "application/json;charset=UTF-8"
+		                        },
+								data: JSON.stringify({
+									script: "data that need"
+								})
+		                    }).success(function (data, status, headers, config) {
+		                        console.log('result:', data);
+								var result = JSON.parse(data);
+		                    }).error(function (data, status, headers, config) {
+		                        console.log('get question error');
+		                    });
 						} else {
 							console.log('please wait');
 						}
                     };
 
 					$scope.run = function () {
-
+						if ($scope.assignment_id && $scope.question_id) {
+							var path = "base" + "api/question/test" + $scope.assignment_id +'-' + $scope.question_id;
+							$http({
+		                        method: 'POST',
+		                        url: path,
+		                        headers: {
+		                            "Content-type": "application/json;charset=UTF-8"
+		                        },
+								data: JSON.stringify({
+									script: "script"
+								})
+		                    }).success(function (data, status, headers, config) {
+		                        console.log('result:', data);
+								var result = JSON.parse(data);
+		                    }).error(function (data, status, headers, config) {
+		                        console.log('get question error');
+		                    });
+						} else {
+							console.log('please wait');
+						}
 					};
 
 					$rootScope.$on('question_value', function (value) {
