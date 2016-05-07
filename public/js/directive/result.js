ca80f4d5ccf049c97acd9168ef3f8f9b086500e4
Preecha Nakngern <preecha@readyplanet.com>
2016-05-07 19:55:59 +0700

    update send request to run

--- public/js/directive/result.js
+++ public/js/directive/result.js
@@ -13,12 +13,20 @@ define([
                 },
                 controller: function ($rootScope, $scope, $http) {
                     $scope.point = 0;
+					$scope.total = 0;
+					$scope.result = false;
 					$scope.question_id = "";
 					$scope.assignment_id = "";
 					//$rootScope.params.question_id
-                    $scope.submit = function () {
+                    $scope.run = function () {
 						if ($scope.assignment_id && $scope.question_id) {
-							var path = "path to submit result of compile";
+							/*
+							var data = {
+								results: true
+							};
+							$scope.result = data.results?1:2;
+							*/
+							var path = "http://172.20.10.11:8090/api/test/compile";
 							$http({
 		                        method: 'POST',
 		                        url: path,
@@ -26,11 +34,13 @@ define([
 		                            "Content-type": "application/json;charset=UTF-8"
 		                        },
 								data: JSON.stringify({
-									script: "data that need"
+									script: "print 'hello, world'",
+									api: $scope.assignment_id,
+									qid: $scope.question_id
 								})
 		                    }).success(function (data, status, headers, config) {
 		                        console.log('result:', data);
-								var result = JSON.parse(data);
+								$scope.result = data.results?1:2;
 		                    }).error(function (data, status, headers, config) {
 		                        console.log('get question error');
 		                    });
@@ -39,9 +49,22 @@ define([
 						}
                     };
 
-					$scope.run = function () {
+					$scope.submit = function () {
 						if ($scope.assignment_id && $scope.question_id) {
-							var path = "http://localhost:8090/api/question/test";
+							/*
+							var data = {
+								tested : [true, false, false]
+							};
+							var point = 0;
+							var testcast = data.tested.length;
+							data.tested.forEach(function (item) {
+								if (item)
+									point++;
+							});
+							console.log(point, testcast);
+							$scope.point = parseInt(point*10/testcast);
+							*/
+							var path = "http://172.20.10.11:8090/api/question/test";
 							$http({
 		                        method: 'POST',
 		                        url: path,
@@ -55,7 +78,14 @@ define([
 								})
 		                    }).success(function (data, status, headers, config) {
 		                        console.log('result:', data);
-								var result = JSON.parse(data);
+								var point = 0;
+								var testcast = data.tested.length;
+								data.tested.forEach(function (item) {
+									if (item)
+										point++;
+								});
+								console.log(point, testcast);
+								$scope.point = parseInt(point*10/testcast);
 		                    }).error(function (data, status, headers, config) {
 		                        console.log('get question error');
 		                    });
@@ -67,6 +97,7 @@ define([
 					$rootScope.$on('question_value', function (value) {
 						if (value) {
 							$scope.question_id = $rootScope.question.aq_id;
+							$scope.total = $rootScope.question.question_point;
 							$scope.assignment_id = $rootScope.assignment.assignment_id;
 						}
 					});
